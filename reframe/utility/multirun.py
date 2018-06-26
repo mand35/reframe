e787ef3250d818700a4cd626149764def2f2a52a
mand35 <mandes@mail.de>
2018-06-26 22:07:34 +0000

    correct multirun

--- reframe/utility/multirun.py
+++ reframe/utility/multirun.py
@@ -6,12 +6,23 @@ def multirun(template_class):
 # name as first input parameter
 # multirun_san_pat: the single sanity pattern
 # multirun_perf_pat: the dict of performance pattern
+# multirun_ref: the (single) reference with the target average value
 # multirun_pre_run: to be added before each run (e.g. time meassure)
 # multirun_post_run: to be added after each run (e.g. time meassure) 
    class MultiRunCheck(template_class):
-       multi_rep = 3
-       def __init__(self, name, **kwargs):
-          super().__init__('{0}_{1}runs'.format(name,self.multi_rep), **kwargs)
+       multi_rep = 11
+       def __init__(self, name, *args, **kwargs):
+          if name is not '':
+             name += '_'
+          super().__init__('{0}{1}runs'.format(name,self.multi_rep), 
+                           *args, **kwargs)
+
+          # scale the assumed runtime
+          self.time_limit = (self.time_limit[0]*self.multi_rep+
+                                int((self.time_limit[1]*self.multi_rep)/60), 
+                             (self.time_limit[1]*self.multi_rep) % 60+
+                                int((self.time_limit[2]*self.multi_rep) /60), 
+                             (self.time_limit[2]*self.multi_rep) % 60)
 
           # check if we got #multi_rep the the sanity patern
           if hasattr(self, 'multirun_san_pat'):
@@ -21,12 +32,13 @@ def multirun(template_class):
           # create the list of result values: first the average and  
           #   then all single elements (to be stored)
           if hasattr(self, 'multirun_perf_pat'):
-             key = list(self.multirun_perf_pat.keys())[0]
-             perf_list = sn.extractall(*self.multirun_perf_pat[key])
              self.perf_patterns = {}
-             self.perf_patterns[key] = sn.avg(perf_list)
-             for run in range(0,self.multi_rep):
-                self.perf_patterns[key+"_{}".format(run)] = perf_list[run]
+             for key in list(self.multirun_perf_pat.keys()):
+                self.perf_patterns[key] = sn.avg(
+                   sn.extractall(*(self.multirun_perf_pat[key])))
+                for run in range(0,self.multi_rep):
+                   self.perf_patterns[key+"_{}".format(run)] = sn.extractall(
+                      *(self.multirun_perf_pat[key]))[run]
    
        # run the test #multi_rep times
        def setup(self, partition, environ, **job_opts):
@@ -50,12 +62,13 @@ def multirun(template_class):
               self.multirun_pre_run = ' '
            if not hasattr(self, 'multirun_post_run'):
               self.multirun_post_run = ' '
-           launch_cmd = ' '.join(self.job.launcher.command(self.job))
+           launch_cmd = (' '.join(self.job.launcher.command(self.job)) + ' ' + 
+                         ' '.join(self.job.launcher.options))
            opts = ' '.join(self.executable_opts)
            for i in range(0,self.multi_rep-1):
-              self.pre_run += (self.multirun_pre_run + 
-                  [launch_cmd + " " + self.executable + opts] + 
-                  self.multirun_post_run)
+              self.pre_run += [*self.multirun_pre_run , (launch_cmd + ' ' + 
+                                  self.executable + ' ' + opts), 
+                                  *self.multirun_post_run]
            self.pre_run += self.multirun_pre_run
            self.post_run += self.multirun_post_run
    return MultiRunCheck
